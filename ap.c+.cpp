6df87f494d7cdf7f117e06ef6efa3bdfc3978ea7
TIRTH39 <130666953+TIRTH39@users.noreply.github.com>
2023-04-13 18:21:36 +0530

    Add files via upload

---
+++ ap.c+.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+	int a=82,b=21,c;
+	c=a+b;
+	printf("addition of %d & %d = %d",a,b,c);
+	c=a-b;
+	printf("\n substruction of %d & %d = %d",a,b,c);
+	c=a*b;
+	printf("\n multiplication of %d & %d = %d",a,b,c);
+	c=a/b;
+	printf("\n dividion of %d & %d = %d",a,b,c);
+	c=a%b;
+	printf("\n module of %d & %d = %d",a,b,c);
+}
